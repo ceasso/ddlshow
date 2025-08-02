6e208cd4beec42a262c7e8451c05d87a68a0bec5
Trocedge <Xfan0202@163.com>
2025-08-02 23:33:34 +0800

    update title and remove over files

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    publicPath: '/',  // 使用自定义域名时应该使用根路径
+    publicPath: '/',  // 使用自定义域名时应该使用根路径, 本地开发环境使用相对地址
     assetsDir: 'static',
     pages: {
         index: {
             entry: 'src/main.js',
             template: 'public/index.html',
             filename: 'index.html',
-            title: '学术活动日历',
+            title: '活动日历',
         }
     }
 }
